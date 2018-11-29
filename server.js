b688d71bc9cd05e00093dfa2a5abcbaa47fec6d3
sunbobin <freehere107@hotmail.com>
2018-11-29 18:20:53 +0800

    address verify

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let PROTO_PATH = __dirname + '/protos/web3.proto';
 let grpc = require('grpc');
 let eth_sign = require(__dirname + '/lib/eth_sign');
+let addressVerify = require(__dirname + '/lib/ethDerivedAddressVerify');
 let land = require(__dirname + '/lib/land');
 let web3_proto = grpc.load(PROTO_PATH).web3;
 let fs = require('fs');
@@ -13,10 +14,14 @@ function RecoverSignedTypeMsg(call, callback) {
     callback(null, {message: eth_sign.ethRecoverTypedSign(call.request.msg, call.request.signed)});
 }
 
+function EthDerivedAddressVerify(call, callback) {
+    callback(null, {message: addressVerify.ethDerivedAddressVerify(call.request.extendedPublicKey, call.request.path, call.request.address)});
+}
+
 function DecodeTokenId(call, callback) {
     let landAbi = JSON.parse(fs.readFileSync(__dirname + '/contract/land.json', 'utf8'));
     land.decodeTokenId(landAbi, call.request.address, call.request.tokenId, (result) => {
-        console.log("result",result)
+        console.log("result", result)
         callback(null, {message: result});
     })
 }
@@ -26,7 +31,8 @@ function main() {
     server.addService(web3_proto.EthWeb3.service, {
         SignedTypeMsg: SignedTypeMsg,
         RecoverSignedTypeMsg: RecoverSignedTypeMsg,
-        DecodeTokenId: DecodeTokenId
+        DecodeTokenId: DecodeTokenId,
+        EthDerivedAddressVerify: EthDerivedAddressVerify
     });
     server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
     server.start();
